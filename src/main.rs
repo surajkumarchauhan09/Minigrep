7517c96527b388eac475292f67330ac0c453e4c7
Suraj Chauhan <surajchauhan.dev@gmail.com>
2026-01-21 13:51:13 +0530

    implimenting search functionality

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::error::Error;
 use std::fs;
 use std::process;
+
+use minigrep::search;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -23,7 +25,9 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents =
         fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}")
+    }
 
     Ok(())
 }
